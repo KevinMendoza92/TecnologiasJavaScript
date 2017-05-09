4cc2804f7e1f5854ab9e0dca99fe626d8cc401c1
KevinMendoza92 <USRDEL@USR19.local>
2017-05-09 15:39:07 -0500

    Sentencias de control
    Ejemplos if, else y switch

---
+++ 02-javascript/Sentencias De Control/SDC.js
@@ -0,0 +1,57 @@
+/*
+var conHambre = false;
+
+if(conHambre){
+    console.log("como");
+}else{
+    console.log("trabajar");
+}*/
+
+
+/*Instanciar un objeto vacio
+* var conHambre={}*/
+/*0=falso
+* 1=verdadero*/
+
+//con un arreglo
+/*var conHambre=[];
+
+if(conHambre.length){
+    console.log("como");
+}else{
+    console.log("trabajar");
+}*/
+
+/*
+var conHambre={nombre:"Kevin"};
+
+if(conHambre.apellido.primero){
+    console.log("como");
+}else{
+    console.log("trabajar");
+} Este es un error que da por poner .primero
+*/
+
+//Con un objeto
+/*
+var conHambre={nombre:"Kevin"};
+
+if(conHambre){
+    console.log("como");
+}else{
+    console.log("trabajar");
+}*/
+
+//Switch
+
+var numero = undefined;
+switch (numero){
+    case 1:
+        console.log("Es uno");
+        break;
+    case 5:
+        console.log("Es cinco");
+        break;
+    default:
+        console.log("No es ni el 1 ni el 5");
+}
