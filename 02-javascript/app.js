5ea50ef0b74e61b1bbed056c3f6a915966787ba2
KevinMendoza92 <USRDEL@USR19.local>
2017-05-09 16:05:50 -0500

    ejemplo de un objeto
    objeto que tiene una función dentro

--- 02-javascript/app.js
+++ 02-javascript/app.js
@@ -1,5 +1,20 @@
-/**
- * Created by USRDEL on 3/5/17.
- */
 
 console.log('Hello world');
+
+var kevin = {
+    nombre:"Kevin",
+    apellido:"Mendoza",
+    fechaNacimiento: new Date(),
+    imprimir: function (){
+            console.log(this.nombre, this.apellido);
+        },
+        anio:function () {
+            return this.fechaNacimiento.getFullYear();
+        },
+        diferenciaEdad: function (edadAComparar) {
+            return Math.abs(edadAComparar-this.fechaNacimiento.getFullYear());
+        }
+    }
+    kevin.imprimir();
+    console.log(kevin.anio());
+    console.log(kevin.diferenciaEdad(1992));
