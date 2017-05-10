69e3f4aa25a1a0bf6eb8ee0de3110f16eeb91d98
KevinMendoza92 <USRDEL@USR19.local>
2017-05-10 14:50:59 -0500

    directorio some y every
    ejemplos del uso

---
+++ 02-javascript/some y every/someEvery.js
@@ -0,0 +1,16 @@
+//some y every
+//some parecido al operador logico or
+//every parecido al operador and
+let arreglo = [8,6,9,10,4,2,7,8];
+
+let notasDeSupletorio = arreglo.map((valor,indice)=>{
+        return valor/5;
+})
+.every((valor)=>{
+    return valor=1;
+});
+
+console.log(notasDeSupletorio);
+
+
+
