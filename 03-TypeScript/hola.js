40afdbb0c48e04cbf2a533d6ae1029be39bcdd78
KevinMendoza <USRDEL@USR19.local>
2017-05-17 13:40:37 -0500

    Correcciones de la carpeta 04-Angular

--- 03-TypeScript/hola.js
+++ 03-TypeScript/hola.js
@@ -1,7 +1,40 @@
-var holaMundo = "Que tal mundo cruel";
+/**
+ * Created by usrdel on 16/5/17.
+ */
+var holaMundo = "holaaaaa";
 console.log(holaMundo);
-/*let decimal: number = 6;
-let hex: number = 0xf00d;
-let binary: number = 0b1010;
-let octal: number = 0o744;
-*/ 
+var soltero = true; //or false
+var decimal = 54;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+//String
+var color = "rojo";
+//Arrays
+var list = [1, 2, 3];
+var list2 = [1, 2, 3];
+var Persona = (function () {
+    //?: significa q son parametros opcionales y ya no  necesimamos: let adrian:Persona = new Persona(); inicializarla
+    function Persona(nombre, apellido, fechaNacimiento) {
+        this.nombre = "Nom"; //pubico por defectp+o
+    }
+    return Persona;
+}());
+/*  private imprimirPersona(nombre:string, apellido?:string:void){
+ in(apellido){
+ console.log(nombre, apellido)
+ }
+ //imprimirPersona(){
+ //console.log(this.nombre, this.apellido);
+ }
+ }
+ let adrian:Persona = new Persona();
+ */
+//Funciones
+function SumaNumero(numero1, numero2) {
+    return numero1 + numero2;
+}
+//Se puede escribir texto directo de javaScript
+var objeto = {
+    nombew: "kevin"
+};
--- 03-TypeScript/hola.ts
+++ 03-TypeScript/hola.ts
@@ -1,57 +1,49 @@
 /**
- * Created by USRDEL on 16/5/17.
+ * Created by usrdel on 16/5/17.
  */
+let holaMundo=`holaaaaa`;
 
+console.log(holaMundo);
 
-//let holamundo = `Que tal mundo cuel`;
-//console.log("hola mundo");
+let soltero: boolean = true; //or false
+let decimal: number = 54;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+//String
+let color: String = "rojo";
+//Arrays
+let list: number[] = [1, 2, 3];
 
-// boolean
-let soltero:boolean = true;  //false
+let list2: Array<number> = [1, 2, 3];
 
-//enteros
-let novias =12;
+class Persona {
+    public nombre: string = "Nom"; //pubico por defectp+o
+    private apellido: string;
+    public fechaNacimiento: Date;
 
-//string
-let frasetion = "You´re done";
-let otraFrasetion = `El numero de novias es ${novias +1}`;
+//?: significa q son parametros opcionales y ya no  necesimamos: let adrian:Persona = new Persona(); inicializarla
+    constructor(nombre?: string,
+                apellido?: string,
+                fechaNacimiento?: Date) {
 
-
-//arrays
-
-let notas: number[]=[1,2,34] //[]
-let jaladas:Array<number>=[1,2,3] //[]
-
-
-let nombre:any = "asd"
-nombre = 12;
-
-let usuario: UsuarioLogin ={
-    nombre:"Kevin",
-    apellido:"Mendoza",
-    direccion:"Panamericana norte"
-
-}
-
-
-interface UsuarioLogin{
-    nombre:string,
-    apellido:string,
-    direccion?: String   // el ? hace opcional el atributo
-}
-
-class persona {
-    public nombre:string = "Nom"; //pubico por defectp+o
-    private apellido:string ;
-    public fechaNacimiento:Date;
-
-
-    constructor(nombre:string,
-                apellido:string,
-                fechaNacimiento?:Date){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.fechaNacimiento = fechaNacimiento;
     }
 }
-
+/*  private imprimirPersona(nombre:string, apellido?:string:void){
+ in(apellido){
+ console.log(nombre, apellido)
+ }
+ //imprimirPersona(){
+ //console.log(this.nombre, this.apellido);
+ }
+ }
+ let adrian:Persona = new Persona();
+ */
+//Funciones
+function SumaNumero(numero1:number, numero2:number):number{
+    return numero1 + numero2;
+}
+//Se puede escribir texto directo de javaScript
+var objeto ={
+    nombew:"Kevin"
+};
