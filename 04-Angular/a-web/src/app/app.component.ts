6cc554cacf50d4fc96aef9611f98764710658539
KevinMendoza <USRDEL@USR19.local>
2017-05-17 14:32:15 -0500

    código en app.components.ts

--- 04-Angular/a-web/src/app/app.component.ts
+++ 04-Angular/a-web/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'twj-raiz',
+  template: `
+  <h1>Hola</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenido!';
-  nombre:string ="Kevin";
-  apellido:string="Mendoza";
+  usuario: UsuarioInterface = {
+    nombre: "Kevin",
+    apellido: "Mendoza"
+  }
+
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+  }
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+  saludar(nombre: String,
+          apellido?:String):String{
+    return `Hola ${nombre} ${apellido}`;
+  }
+}
+
+interface  UsuarioInterface{
+  nombre : String,
+  apellido : String
 }
