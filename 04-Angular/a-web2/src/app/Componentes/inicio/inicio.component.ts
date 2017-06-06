60540e436dfc55801a87df9bdd7b7662c67e9089
KevinMendoza <patricio.mendoza@epn.edu.ec>
2017-06-06 00:36:54 -0500

    Deber planetas

--- 04-Angular/a-web2/src/app/Componentes/inicio/inicio.component.ts
+++ 04-Angular/a-web2/src/app/Componentes/inicio/inicio.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Http} from "@angular/http";
+
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -8,46 +10,63 @@ import 'rxjs/add/operator/map';
 })
 export class InicioComponent implements OnInit {
 
-  nombre: string = "Kevin";
+  nombre = 'Kevin';
+  planetas: PlanetaStarWars;
+  // cmmand + a y luego command + alt + l
+  arregloImagenes = [
+    'https://vignette1.wikia.nocookie.net/es.starwars/images/4/4a/Alderaan.jpg/revision/latest?cb=20100723184830',
+    'https://vignette4.wikia.nocookie.net/es.starwars/images/a/a0/Eaw_Yavin4.jpg/revision/latest?cb=20080127231835',
+    'https://vignette4.wikia.nocookie.net/es.starwars/images/8/80/Hoth2.jpg/revision/latest?cb=20070317174056',
+    'https://vignette1.wikia.nocookie.net/es.starwars/images/1/1c/Dagobah.jpg/revision/latest?cb=20061117132132',
+    'https://vignette1.wikia.nocookie.net/es.starwars/images/8/80/Bespin_TESB.png/revision/latest?cb=20170528000216',
+    'https://vignette1.wikia.nocookie.net/es.starwars/images/e/e1/Endor2.JPG/revision/latest?cb=20130907135939',
+    'https://vignette4.wikia.nocookie.net/es.starwars/images/5/50/Naboo.jpg/revision/latest?cb=20060318190228',
+    'https://vignette3.wikia.nocookie.net/es.starwars/images/1/17/Coruscant-AoTCW.jpg/revision/latest?cb=20100213220520',
+    'https://vignette2.wikia.nocookie.net/es.starwars/images/5/52/Kamino-DB.png/revision/latest?cb=20160913200828',
+    'https://vignette2.wikia.nocookie.net/es.starwars/images/6/6d/Geonosis_AotC.png/revision/latest?cb=20161025214407'
+  ];
 
-  planetas=[];
   /*planetas[planetaStarWars]=[]
-  interface planetaStarWars{
-    name: string,....etc
-}*/
+   interface planetaStarWars{
+   name: string,....etc
+   }*/
   //comand+a y luego command command+alt+l
-  arregloUsuario = [
+
+  arregloUsuario = [{
+    nombre: 'Kevin',
+    apellido: 'Mendoza',
+    conectado: true
+  },
     {
-      nombre: "Kevin",
-      apellido: "Mendoza",
+      nombre: 'K',
+      apellido: 'M',
       conectado: true
-    }, {
-      nombre: "K",
-      apellido: "M",
-      conectado: true
-    }, {
-      nombre: "E",
-      apellido: "R",
-      conectado: true
-    }
-  ]
+    },
+    {
+      nombre: 'E',
+      apellido: 'R',
+      conectado: false
+    }];
 
   constructor(private _http: Http) {
+    //Inicia la clase
+    //PERO EL COMPONENTE NO ESTA LISTO!!!
   }
 
   ngOnInit() {
+    //Esta listo el componente
   }
 
   //sintaxis de tipo void
   cambiarNombre(): void {
-    this.nombre = "Rafico a Lenin ";
+    this.nombre = 'Rafico a Lenin';
   }
 
-  cambiarOtroNombre() {
-    this.nombre = "Lenin a Rafico"
+  cambiarOtroNombre(): void {
+    this.nombre = 'Lenin a Rafico';
   }
 
-  cambianNombreInput(nombreEtiqueta) {
+  cambiarNombreInput(nombreEtiqueta) {
     console.log(nombreEtiqueta.value);
     console.log(nombreEtiqueta.type);
     console.log(nombreEtiqueta.placeholder);
@@ -55,27 +74,41 @@ export class InicioComponent implements OnInit {
   }
 
   //el _(ejemplo) significa que es privado
-  cargarPlanetas(){
-    this._http.get("swapi.co/api/planets")
-
-      //.map(response=>response.json())
+  cargarPlanetas() {
+    this._http
+      .get('http://swapi.co/api/planets')
+      // .map(response => response.json())
       .subscribe(
-        (response)=>{
-          console.log("Response", response);
-          console.log(response.json());
-          let respuesta = response.json();
-          console.log(respuesta.next);
-          this.planetas=respuesta.result;
+        (reponse) => {
+          console.log('Response: ', reponse);
+          console.log(reponse.json());
+          let repuesta = reponse.json();
+          console.log(repuesta.next);
+          this.planetas = repuesta.results;
         },
-        (error)=>{
-          console.log("error", error);
+        (error) => {
+          console.log('Error: ', error);
         },
-        ()=>{
-          console.log("finally");
+        () => {
+          console.log('Finally');
         }
-      )
+      );
   }
+}
 
-
-
+interface PlanetaStarWars {
+  name: string;
+  rotation_period: number;
+  orbital_period: number;
+  diameter: number;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: number;
+  population: number;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
 }
